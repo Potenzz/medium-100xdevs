4a7e4a3ef8d82e5343be8d07106608f8e706edd4
Vishnu <vishnu10kumar11@gmail.com>
2024-12-29 01:04:46 +0530

    cors added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono';
 import v1Routes from './routes/v1/index';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
+
+app.use("*", cors());
 app.route('/api/v1', v1Routes);
 
 export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,3 @@
-// import { Hono } from 'hono'
-// const app = new Hono()
-
-// app.get('/', (c) => c.text('Hono!'))
-
-// export default app
-
 import app from './app';
 
 export default app;
